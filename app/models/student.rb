3932b623de2faded7553b457da52601862c86161
Mutiso <suzanmutiso@gmail.com>
2023-08-13 21:20:54 +0300

    Completed

---
+++ app/models/student.rb
@@ -0,0 +1,8 @@
+class Student < ApplicationRecord
+     def to_s
+        "#{first_name} #{last_name}"
+     end
+end
+
+student = Student.new(first_name: 'Dwayne', last_name: 'Johnson')
+puts student.to_s
